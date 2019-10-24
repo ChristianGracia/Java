a51abbc69a4598f32411d5ad19b909ce9b82f223
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-23 23:44:12 -0400

    sql query set up and db added

--- src/jsonToJava.java
+++ src/jsonToJava.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class jsonToJava {
 	public static void main(string[] args) {
@@ -8,7 +9,8 @@ public class jsonToJava {
 		DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "Christian12!");
 	    
 		//object of statement to execute queries
-		conn.createStatement()
+		Statement st=conn.createStatement();
+		st.executeQuery("select * from CustomerInfo where purchasedDate=CURDATE() and Location ='Asia';");
 	}
 
 }
