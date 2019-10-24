e714db470ddbd246fe643b7d283bf9bf09ed27be
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-24 00:32:10 -0400

    coin detail class created and property getters and setters set

---
+++ dbToJSON/CoinDetails.java
@@ -0,0 +1,32 @@
+
+public class CoinDetails {
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public String getPurchaseDate() {
+		return PurchaseDate;
+	}
+	public void setPurchaseDate(String purchaseDate) {
+		PurchaseDate = purchaseDate;
+	}
+	public int getAmount() {
+		return Amount;
+	}
+	public void setAmount(int amount) {
+		Amount = amount;
+	}
+	public String getLocation() {
+		return Location;
+	}
+	public void setLocation(String location) {
+		Location = location;
+	}
+	private String courseName;
+	private String PurchaseDate;
+	private int Amount;
+	private String Location;
+
+}
