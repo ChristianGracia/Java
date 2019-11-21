3e2d9f112a7bead85d344728f72fb4fe73c928b1
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-21 00:25:32 -0500

    removed program

--- templates/springboot/sql/scraper/src/main/java/com/cg/scraper/CoinController.java
+++ templates/springboot/sql/scraper/src/main/java/com/cg/scraper/CoinController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ public class CoinController {
 	@Autowired
 	CoinRepository coinRepository;
 	
+	   @CrossOrigin(origins = "http://localhost:3000")
 	   @GetMapping("/all")
 	   public List<CoinInfo> getAllCoins() {
 		   
