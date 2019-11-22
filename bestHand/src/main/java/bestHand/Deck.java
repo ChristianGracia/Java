649f3093285333ee2d997e85b1f4b09018475d68
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-21 19:12:05 -0500

    game classes more set up

--- bestHand/src/main/java/bestHand/Deck.java
+++ bestHand/src/main/java/bestHand/Deck.java
@@ -1,5 +1,39 @@
 package bestHand;
 
+import java.util.Arrays;
+
 public class Deck {
+	private Card[] deck;
+	
+	public Deck() {
+		this.setDeck(createDeck());	
+		System.out.println(Arrays.toString(deck));
+	}
+
+	public Card[] getDeck() {
+		return deck;
+	}
+
+	public void setDeck(Card[] deck) {
+		this.deck = deck;
+	}
+	
+	private Card[] createDeck() {
+		Card[] newDeck = new Card[52];
+		
+		for(int i = 1, j = 0, k=1; k < 53 ; i++, j++, k++ ) {
+			if(j % 4 == 0) {
+				j = 0;
+			}
+			if(i % 14 == 0) {
+				i = 1;
+			}
+			deck[k] = new Card(i, j, k);
+			
+		}
+	
+		return newDeck;
+		
+	}
 
 }
---
+++ bestHand/src/main/java/bestHand/Main.java
@@ -0,0 +1,9 @@
+package bestHand;
+
+public class Main {
+
+	public static void Main(String[] args) {
+		Deck deck = new Deck();
+	}
+
+}
