0e5d31e6568eed7e7b1e55b07ecb8425cc463675
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-25 18:58:57 -0500

    gofish game in java init

---
+++ goFish/src/main/java/goFish/Card.java
@@ -0,0 +1,33 @@
+package goFish;
+
+public class Card {
+	private int value;
+	private int suit;
+	private int numberOfCards;
+	
+	public Card(int value, int suit, int numberOfCards){
+		setValue(value);
+		setSuit(suit);
+		setCardNumber(numberOfCards);
+		
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public int getSuit() {
+		return suit;
+	}
+	public void setSuit(int suit) {
+		this.suit = suit;
+	}
+	public int getCardNumber() {
+		return numberOfCards;
+	}
+	public void setCardNumber(int cardNumber) {
+		this.numberOfCards = cardNumber;
+	}
+
+}
---
+++ goFish/src/main/java/goFish/Main.java
@@ -0,0 +1,10 @@
+package goFish;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Deck deck = new Deck();
+	
+	}
+
+}
