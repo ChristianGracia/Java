be8aa68feb37d65508a5985c8a3ddfd7193e12c5
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-25 23:38:27 -0500

    code clean

--- goFish/src/main/java/goFish/Game.java
+++ goFish/src/main/java/goFish/Game.java
@@ -14,16 +14,6 @@ public class Game {
 		Player player1 = new Player("player 1", deck.dealHand(deck.getDeck(), 5));
 		Player player2 =  new Player("player 2", deck.dealHand(deck.getDeck(), 5)); 
 		startTurn(player1);
-		
-//		for(Card item : player2.getCards()) {
-//		System.out.print(item.getCardNumber());
-//		System.out.print(" ");
-//		System.out.print(item.getValue());
-//		System.out.print(" ");
-//		System.out.print(item.getSuit());
-//		System.out.println(deck.getDeck().size());
-//
-//	}
 	}
 
 	private void startTurn(Player player) {
@@ -34,8 +24,6 @@ public class Game {
 			System.out.print(card.getValue() + " " +  card.getSuit() + "     ");
 		}
 		System.out.println("\n");
-		
-		
 
 	}
 
@@ -76,6 +64,5 @@ public class Game {
 //		   return false;
 //		  }
 //		 }
-
 }
 
