9d4a42a446b5a71d795cc516c082c2859ca5f24a
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-27 16:32:16 -0500

    added resuable card class

---
+++ gameClasses/Card.java
@@ -0,0 +1,31 @@
+public class Card {
+	private int value;
+	private String suit;
+	private int numberOfCards;
+	
+	public Card(int value, String suit, int numberOfCards){
+		setValue(value);
+		setSuit(suit);
+		setCardNumber(numberOfCards);
+		
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public String getSuit() {
+		return suit;
+	}
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+	public int getCardNumber() {
+		return numberOfCards;
+	}
+	public void setCardNumber(int cardNumber) {
+		this.numberOfCards = cardNumber;
+	}
+
+}
