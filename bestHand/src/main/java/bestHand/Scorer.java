9dd535aa2e9ce8645d131ecf3c9c122c809b1508
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-01 20:48:30 -0500

    score class being set up

--- bestHand/src/main/java/bestHand/Scorer.java
+++ bestHand/src/main/java/bestHand/Scorer.java
@@ -2,4 +2,34 @@ package bestHand;
 
 public class Scorer {
 	
+	private Player[] players;
+	
+	public Scorer(Player[] players) {
+		setPlayers(players);
+		
+	}
+
+	public Player[] getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(Player[] players) {
+		this.players = players;
+	}
+	
+	private void calculateSum(Player player) {
+		int sum = 0;
+		for(Card card : player.getCards()) {
+			sum += card.getValue();
+		}
+		System.out.println(player.getName() + ":  Sum of cards: " + sum);
+	}
+//	private void removeFaceCards(Player player) {
+//		int sum = 0;
+//		for(Card card : player.getCards()) {
+//			sum += card.getValue();
+//		}
+//		System.out.println(player.getName() + ":  Sum of cards: " + sum);
+//	}
+
 }
