87c7224c317061cbb23be4abff351773c3269ed5
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-09 19:56:21 -0500

    changed var names for readability in deck creation loop

--- goFish/src/main/java/goFish/Deck.java
+++ goFish/src/main/java/goFish/Deck.java
@@ -16,14 +16,14 @@ public class Deck {
 	   Card[] temp = new Card[52];
 	   String[] suitArr =  getSuitArray();
 		
-		for(int i = 1, j = 0, k=0; k < 52 ; i++, j++, k++ ) {
-			if(j % 4 == 0) {
-				j = 0;
+		for(int value = 1, suit = 0, numberOfCards=0; numberOfCards < 52 ; value++, suit++, numberOfCards++ ) {
+			if(suit % 4 == 0) {
+				suit = 0;
 			}
-			if(i % 14 == 0) {
-				i = 1;
+			if(value % 14 == 0) {
+				value = 1;
 			}
-			temp[k] = new Card(i, suitArr[j]);
+			temp[numberOfCards] = new Card(value, suitArr[suit]);
 		}
 		ArrayList<Card> cards = new ArrayList<Card>();
 		
