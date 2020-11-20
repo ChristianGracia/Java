8e631c8f2698a4b419183f35649f90b822c75753
Christian Gracia <christiangracia.student@careerdevs.com>
2020-11-19 22:12:01 -0500

    added exmaple using a generic vector and a comment about thread saftey

--- DataStructures/Vectors/VectorGeneric/src/com/cg/Main.java
+++ DataStructures/Vectors/VectorGeneric/src/com/cg/Main.java
@@ -2,15 +2,18 @@ package com.cg;
 
 import java.util.ArrayList;
 import java.util.List;
-
-
-//not thread safe
+import java.util.Vector;
 
 public class Main {
 
+    //synchronized, can be used on different threads without worrying about doing the handling for that
+    //cause overhead so array list used when this isn't concern
+
+    // thread safe
+
     public static void main(String[] args) {
 
-        List<Employee> employeeList = new ArrayList<>();
+        List<Employee> employeeList = new Vector<>();
 
         employeeList.add(new Employee("cc", "d", 123));
         employeeList.add(new Employee("dd", "l", 126));
