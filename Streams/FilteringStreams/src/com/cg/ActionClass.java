e916374fa9ba76356e77f3a280f1d15698d9c32c
Christian Gracia <christiangracia.student@careerdevs.com>
2020-12-22 23:59:58 -0500

    Added stream filter example using a predicate

--- Streams/FilteringStreams/src/com/cg/ActionClass.java
+++ Streams/FilteringStreams/src/com/cg/ActionClass.java
@@ -1,6 +1,7 @@
 package com.cg;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class ActionClass {
 
@@ -9,6 +10,11 @@ public class ActionClass {
         List<Post> list = List.of(new Post("a", 5), new Post("b", 10), new Post("c", 20));
 
         list.stream()
-                .filter(post -> post.getLikes() > 10).filter()
+                .filter(post -> post.getLikes() > 10).forEach(post -> System.out.println(post.getTitle()));
+
+        Predicate<Post> isPopular = p -> p.getLikes() > 10;
+
+        list.stream()
+                .filter(isPopular).forEach(post -> System.out.println(post.getTitle()));
     }
 }
