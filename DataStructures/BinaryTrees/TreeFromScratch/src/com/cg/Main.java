91296a67cc7b47e4ccbd08e6ebe2a73bb84319c2
Christian Gracia <christiangracia.student@careerdevs.com>
2021-01-04 21:48:59 -0500

    added a simple binary tree made from scratch with an insert method in java

---
+++ DataStructures/BinaryTrees/TreeFromScratch/src/com/cg/Main.java
@@ -0,0 +1,17 @@
+package com.cg;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Tree intTree = new Tree();
+
+        intTree.insert(23);
+        intTree.insert(66);
+        intTree.insert(13);
+        intTree.insert(10);
+        intTree.insert(73);
+
+
+    }
+}
