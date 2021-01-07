67aa142e53c0c3f68e562834ef2ebe3235afee10
Christian Gracia <christiangracia.student@careerdevs.com>
2021-01-06 20:43:47 -0500

    added a delete method, is empty, and a method for fixing the nodes below a heap when the replacement node during deletion is less then its children

--- DataStructures/BinaryTrees/BinaryHeap/src/com/cg/Heap.java
+++ DataStructures/BinaryTrees/BinaryHeap/src/com/cg/Heap.java
@@ -13,11 +13,19 @@ public class Heap {
         return size == heap.length;
     }
 
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
     public int getParent(int index) {
         return (index - 1) / 2;
     }
 
-    public void heapify(int index) {
+    public int getChild(int index, boolean left) {
+        return 2 * index + (left ? 1 : 2);
+    }
+
+    public void fixHeapAbove(int index) {
 
         int newValue = heap[index];
         while (index > 0 && newValue > heap[getParent(index)]) {
@@ -33,9 +41,68 @@ public class Heap {
             throw new IndexOutOfBoundsException("Heap is full");
         }
 
-        heap[size++] = value;
+        heap[size] = value;
 
-        heapify(size);
+        fixHeapAbove(size);
         size++;
+    }
+
+    public int delete(int index) {
+        //always replace with right most left and then fix tree up or down
+
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("Heap is empty");
+        }
+
+        int parent = getParent(index);
+        int deletedValue = heap[index];
+
+        heap[index] = heap[size - 1];
+
+        if (index == 0 || heap[index] < heap[parent]) {
+            fixHeapBelow(index, size - 1);
+        } else {
+            fixHeapAbove(index);
+        }
+
+        size--;
+        return deletedValue;
+
+    }
+
+    private void fixHeapBelow(int index, int lastHeapIndex) {
+
+        int childToSwap;
+
+        while (index <= lastHeapIndex) {
+            int leftChild = getChild(index, true);
+            int rightChild = getChild(index, false);
+
+
+            //has left child if true
+            if (leftChild <= lastHeapIndex) {
+                //has right child if true
+                if (rightChild > lastHeapIndex) {
+                    childToSwap = leftChild;
+                } else {
+                    childToSwap = (heap[leftChild] > heap[rightChild] ? leftChild : rightChild);
+                }
+
+                if (heap[index] < heap[childToSwap]) {
+                    int temp = heap[index];
+                    heap[index] = heap[childToSwap];
+                    heap[childToSwap] = temp;
+
+                } else {
+                    break;
+                }
+                index = childToSwap;
+            } else {
+                break;
+            }
+
+
+        }
+
     }
 }
